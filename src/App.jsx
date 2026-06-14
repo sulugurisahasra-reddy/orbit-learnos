7fc4607578cf2c3d38cb66663f76788842189df7
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 15:46:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function Dashboard() {
-  return <h1>Dashboard</h1>;
-}
-
-function Tracker() {
-  return <h1>Tracker</h1>;
-}
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/tracker" element={<Tracker />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Orbit LearnOS</h1>
+    </div>
   );
 }
 
