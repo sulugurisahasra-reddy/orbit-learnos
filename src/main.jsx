668765a692590265275c2767a7c773d127c34037
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 15:29:03 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "white", background: "black", height: "100vh" }}>
-    React Working 🚀
-  </h1>
-);
+function Test() {
+  return <h1>Hello React 🚀</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Test />);
