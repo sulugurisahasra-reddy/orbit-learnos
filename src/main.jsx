37808f846a25bd3f00b19895e5b0fba4aa7c3f20
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 15:49:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-function App() {
-  return (
-    <h1 style={{ color: "red", fontSize: "50px" }}>
-      ORBIT WORKING
-    </h1>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
