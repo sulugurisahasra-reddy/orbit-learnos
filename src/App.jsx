53dff572da0d704ca129f8bf10fca1fa9f3c8b04
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 14:56:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,15 @@ import Settings from "./pages/Settings";
 
 function App() {
   return (
-
-    <BrowserRouter>
-
+    <BrowserRouter basename="/orbit-learnos">
       <Routes>
-
         <Route path="/" element={<Dashboard />} />
-
         <Route path="/tracker" element={<Tracker />} />
-
         <Route path="/tasks" element={<Tasks />} />
-
         <Route path="/analytics" element={<Analytics />} />
-
         <Route path="/settings" element={<Settings />} />
-
       </Routes>
-
     </BrowserRouter>
-
   );
 }
 
