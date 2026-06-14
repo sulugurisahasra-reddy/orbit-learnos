c817cfb4a1411750ca32f72ece6ce1bf0badea6d
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 14:59:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/orbit-learnos">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
