08d738ddb01db8879d5082d5cd22f6a4764d1739
sulugurisahasra-reddy <suluguri.sahasra@klh.edu.in>
2026-06-14 15:00:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Dashboard from "./pages/Dashboard";
-import Tracker from "./pages/Tracker";
-import Tasks from "./pages/Tasks";
-import Analytics from "./pages/Analytics";
-import Settings from "./pages/Settings";
-
 function App() {
   return (
-    <BrowserRouter basename="/orbit-learnos">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/tracker" element={<Tracker />} />
-        <Route path="/tasks" element={<Tasks />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/settings" element={<Settings />} />
-      </Routes>
-    </BrowserRouter>
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "40px",
+      }}
+    >
+      ORBIT WORKING
+    </div>
   );
 }
 
